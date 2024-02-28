84f454fa3708ca5dacb4cacb98418e4cfd2aff80
ikachaeva <irenkachaeva@gmail.com>
2024-02-28 20:34:02 +0300

    тест в формате unittest (исправлен баз с функцией)

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -9,6 +9,7 @@ class Test_all(unittest.TestCase):
         self.assertEqual(arrs.get([], 0, "test"), "test")
 
 
-def test_slice(self):
-    self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
-    self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+    def test_slice(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs.my_slice([], 1, 3), [])
